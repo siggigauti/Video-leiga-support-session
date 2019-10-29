637df41175e472883dc02adadf70cafbcfd0f5aa
siggigauti <siggigauti@gmail.com>
2019-10-29 12:30:05 +0000

    signup og login

--- src/main/java/is/hi/hbv501/videoleiga/videoleiga/Controllers/HomeController.java
+++ src/main/java/is/hi/hbv501/videoleiga/videoleiga/Controllers/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -106,4 +107,57 @@ public class HomeController {
         return "Velkominn";
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signUpGET(User user){
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signUpPOST(@Valid User user, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "signup";
+        }
+        User exists = userService.findByUName(user.uName);
+        if(exists == null){
+            userService.save(user);
+        }
+        model.addAttribute("movies", movieService.findAll());
+        return "Velkominn";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String usersGET(Model model){
+        model.addAttribute("users", userService.findAll());
+        return "users";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGET(User user){
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPOST(@Valid User user, BindingResult result, Model model, HttpSession session){
+        if(result.hasErrors()){
+            return "login";
+        }
+        model.addAttribute("movies",movieService.findAll());
+        User exists = userService.login(user);
+        if(exists != null){
+            session.setAttribute("LoggedInUser", user);
+            return "redirect:/";
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/loggedin", method = RequestMethod.GET)
+    public String loggedinGET(HttpSession session, Model model){
+        model.addAttribute("movies",movieService.findAll());
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if(sessionUser  != null){
+            model.addAttribute("loggedinuser", sessionUser);
+            return "loggedInUser";
+        }
+        return "redirect:/";
+    }
 }
